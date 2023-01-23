29760d9544d16088a2b2aa0f525e3b32b78cfa24
Shubh Agarwal <shubhagarwa8888@gmail.com>
2023-01-24 00:49:53 +0530

    Update 2.py

--- Lab-2/2.py
+++ Lab-2/2.py
@@ -139,29 +139,7 @@ def ascii_heuristic(initial_state: List[Tuple[int, int, str]],
     return sum
 
 
-def hillClimb(problem: Problem, heuristic: Callable[..., int]) -> bool:
-    """It will return if the end_goal is rechable or not using 
-    the current heuristics, using greedy approach, currently maximizing
-    the heuristics value"""
-    initial_state = problem.get_initial_state()
-    final_state = problem.get_goal_state()
-    current_node = initial_state
-    current_node_heu = heuristic(current_node, final_state)
-    while True:
-        if problem.is_goal_state(current_node):
-            break
-        successors = problem.get_successor(current_node)
-        heuristic_vals = [heuristic(successor, final_state) for successor in successors]
-        max_heu = max(heuristic_vals)
-        if max_heu > current_node_heu:
-            current_node = successors[heuristic_vals.index(max_heu)]
-            current_node_heu = max_heu
-        else:
-            return False
-    return True
-
-
-def hillClimbMod(problem: Problem, heuristic: Callable[..., int]) -> Tuple[int, bool]:
+def hillClimb(problem: Problem, heuristic: Callable[..., int]) -> Tuple[int, bool]:
     """It will return if the end_goal is rechable or not using 
     the current heuristics, using greedy approach, currently maximizing
     the heuristics value"""
@@ -183,3 +161,5 @@ def hillClimbMod(problem: Problem, heuristic: Callable[..., int]) -> Tuple[int, 
         else:
             return (count, False)
     return (count, True)
+
+    

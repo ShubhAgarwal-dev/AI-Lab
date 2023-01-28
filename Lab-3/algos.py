81aec891d8d29a3d45be0a574ea3c746674cea96
Shubh Agarwal <shubhagarwa8888@gmail.com>
2023-01-28 13:10:41 +0530

    partial crossover implemented

---
+++ Lab-3/algos.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+import numpy as np
+
+def get_legal_label(num, child1, child2, part:int, part2:int):
+    index = -1
+    for i in range(part, part2):
+        if num == child1[i]:
+            index = i
+    opp_num = child2[index]
+    if opp_num in child1:
+        get_legal_label(opp_num, child1, child2, part, part2)
+    else :
+        return opp_num
+
+@dataclass
+class GeneticAlgorithm():
+    
+    cities:int = 100
+    
+    def partial_crossover(self, tour1, tour2):
+        child1 = np.zeros(shape=self.cities, dtype=np.int16)
+        child2 = np.zeros(shape=self.cities, dtype=np.int16)
+        partition = self.cities // 3
+        child2[partition:self.cities-partition] = tour1[partition:self.cities-partition]
+        child1[partition:self.cities-partition] = tour2[partition:self.cities-partition]
+        child1[:partition] = tour1[:partition]; child1[self.cities-partition:] = tour1[self.cities-partition:]
+        child2[:partition] = tour2[:partition]; child2[self.cities-partition:] = tour2[self.cities-partition:]
+        for ind, val in enumerate(child1):
+            if np.sum(np.where(child1 == val, 1, 0)) == 2:
+                child1[ind] = get_legal_label(val, child1, child2, partition, self.cities-partition)
+
+        for ind, val in enumerate(child2):
+            if np.sum(np.where(child2 == val, 1, 0)) == 2:
+                child2[ind] = get_legal_label(val, child2, child1, partition, self.cities-partition)
--- Lab-3/tesy.py
+++ Lab-3/tesy.py
@@ -1,4 +1,8 @@
-from  file_reader import array_converter
-from pprint import pprint
+from timeit import default_timer
 
-pprint(array_converter(r"D:\Projects\AI Lab\Lab-3\euc_100"))
+initial_clock = default_timer()
+
+from  file_reader import array_converter
+vals = array_converter(r"D:\Projects\AI Lab\Lab-3\euc_100")
+end_clock = default_timer()
+print(end_clock - initial_clock)

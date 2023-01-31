14c7a070d61bea41b03c2ee3f34e92f522c974ba
sakshamchhimwal <sakshamchhimwal2410@gmail.com>
2023-01-31 15:45:45 +0530

    partial crossover works fine now

--- Lab-3/algos.py
+++ Lab-3/algos.py
@@ -20,29 +20,68 @@ class GeneticAlgorithm():
 
     cities: int = 100
 
-    def partial_crossover(self, tour1, tour2):
-        child1 = np.zeros(shape=self.cities, dtype=np.int16)
-        child2 = np.zeros(shape=self.cities, dtype=np.int16)
-        partition = self.cities // 3
-        child2[partition:self.cities -
-               partition] = tour1[partition:self.cities-partition]
-        child1[partition:self.cities -
-               partition] = tour2[partition:self.cities-partition]
-        child1[:partition] = tour1[:partition]
-        child1[self.cities-partition:] = tour1[self.cities-partition:]
-        child2[:partition] = tour2[:partition]
-        child2[self.cities-partition:] = tour2[self.cities-partition:]
-        for ind, val in enumerate(child1):
-            if ind > partition and ind < self.cities-partition:
-                continue
-            if np.sum(np.where(child1 == val, 1, 0)) >= 2:
-                child1[ind] = get_legal_label(
-                    val, child1, child2, partition, self.cities-partition)
+    # def partial_crossover(self, tour1, tour2):
+    #     child1 = np.zeros(shape=self.cities, dtype=np.int16)
+    #     child2 = np.zeros(shape=self.cities, dtype=np.int16)
+    #     partition = self.cities // 3
+    #     child2[partition:self.cities -
+    #            partition] = tour1[partition:self.cities-partition]
+    #     child1[partition:self.cities -
+    #            partition] = tour2[partition:self.cities-partition]
+    #     child1[:partition] = tour1[:partition]
+    #     child1[self.cities-partition:] = tour1[self.cities-partition:]
+    #     child2[:partition] = tour2[:partition]
+    #     child2[self.cities-partition:] = tour2[self.cities-partition:]
+    #     for ind, val in enumerate(child1):
+    #         if ind > partition and ind < self.cities-partition:
+    #             continue
+    #         if np.sum(np.where(child1 == val, 1, 0)) >= 2:
+    #             child1[ind] = get_legal_label(
+    #                 val, child1, child2, partition, self.cities-partition)
 
-        for ind, val in enumerate(child2):
-            if ind > partition and ind < self.cities-partition:
-                continue
-            if np.sum(np.where(child2 == val, 1, 0)) >= 2:
-                child2[ind] = get_legal_label(
-                    val, child2, child1, partition, self.cities-partition)
-        return (child1, child2)
+    #     for ind, val in enumerate(child2):
+    #         if ind > partition and ind < self.cities-partition:
+    #             continue
+    #         if np.sum(np.where(child2 == val, 1, 0)) >= 2:
+    #             child2[ind] = get_legal_label(
+    #                 val, child2, child1, partition, self.cities-partition)
+    #     return (child1, child2)
+
+    def partial_crossover(self, tour1, tour2) -> tuple[list[int], list[int]]:
+        size: int = self.cities
+        crossover_point_1: int = size//3
+        crossover_point_2: int = 2 * (size//3)
+
+        # MAKING CHILD_1
+        repMap1: dict[int, int] = {}
+        for i in range(crossover_point_1, crossover_point_2+1):
+            repMap1[tour1[i]] = tour2[i]
+        child_1: list[int] = tour2[0: crossover_point_1] + \
+            tour1[crossover_point_1:crossover_point_2+1] + \
+            tour2[crossover_point_2+1:]
+        for i in range(0, crossover_point_1):
+            if (child_1[i] in repMap1.keys()):
+                while (child_1[i] in repMap1.keys()):
+                    child_1[i] = repMap1[child_1[i]]
+        for i in range(crossover_point_2+1, self.cities):
+            if (child_1[i] in repMap1.keys()):
+                while (child_1[i] in repMap1.keys()):
+                    child_1[i] = repMap1[child_1[i]]
+
+        # MAKING CHILD 2
+        repMap2: dict[int, int] = {}
+        for i in range(crossover_point_1, crossover_point_2+1):
+            repMap2[tour2[i]] = tour1[i]
+        child_2: list[int] = tour1[0: crossover_point_1] + \
+            tour2[crossover_point_1:crossover_point_2+1] + \
+            tour1[crossover_point_2+1:]
+        for i in range(0, crossover_point_1):
+            if (child_2[i] in repMap2.keys()):
+                while (child_2[i] in repMap2.keys()):
+                    child_2[i] = repMap2[child_2[i]]
+        for i in range(crossover_point_2+1, self.cities):
+            if (child_2[i] in repMap2.keys()):
+                while (child_2[i] in repMap2.keys()):
+                    child_2[i] = repMap2[child_2[i]]
+
+        return (child_1, child_2)
--- Lab-3/tesy.py
+++ Lab-3/tesy.py
@@ -11,7 +11,8 @@ import time
 # vals = array_converter("D:\AI LAB\AI-Lab\Lab-3\euc_100")
 
 # print(vals[3])
-vals = fileReader("D: \AI LAB\AI-Lab\Lab-3\euc_100")
+vals: tuple[str, int, list[tuple[float, float]], list[list[float]]
+            ] = fileReader("D: \\AI LAB\\AI-Lab\\Lab-3\\euc_100")
 
 population: list[list[int]] = []
 while (len(population) < 100):
@@ -27,11 +28,24 @@ while (len(population) < 100):
 fittest_elements: list[list[int]] = []
 eletism(vals[3], 10, population, fittest_elements)
 
+# for i in fittest_elements:
+#     print(i)
+#     print()
+
 
 for i in range(0, 10, 2):
     algs: GeneticAlgorithm = GeneticAlgorithm(cities=vals[1])
-    crossover_ele = algs.partial_crossover(
+    crossover_ele: tuple[list[int], list[int]] = algs.partial_crossover(
         tour1=fittest_elements[i], tour2=fittest_elements[i+1])
     for i in crossover_ele:
         print(i)
+        print(getFitness(vals[3], i))
         print()
+
+# PARTIAL CROSSOVER TEST CODE
+
+# arr1: list[int] = [1, 2, 5, 6, 4, 3, 8, 7]
+# arr2: list[int] = [1, 4, 2, 3, 6, 5, 7, 8]
+
+# algs: GeneticAlgorithm = GeneticAlgorithm(cities=8)
+# print(algs.partial_crossover(arr1, arr2))
